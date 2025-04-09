7a7acb52d8c62bcb1439d5eb1301e80212542ca6
ChloeHenchoz <chloe.hnchz@eduge.ch>
2025-04-09 11:47:10 +0200

    bon ça marche mieux mais faiut finir le check droit pour qu'il puisse tourner à droite aussi

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ BLACK = 1
 speed = 30
 
 print("depart")
+led_rgb(Color.WHITE)
 #def calibration():
    # time = time-time()
    
-def checkdroite():
+def checkdroite():  # renvoit true si y'a pas de mur 
     V = False
     droite()
-    sleep(1800)
+    sleep(1200)
     motor_stop()
     print("droit")
     if (line_sensor(LineSensor.R1)== WHITE and line_sensor(LineSensor.L1)==WHITE and line_sensor(LineSensor.M)==WHITE):
@@ -24,29 +25,54 @@ def checkdroite():
     sleep(800)
     motor_stop()
     print("check fini ")
-    return True 
+    sleep(500)
+    return (V)
     
-def checkgauche():
+def checkgauche(): # renvoit true si y'a pas de mur 
     V = False
     gauche()
-    sleep(1900)
+    sleep(1000)
     motor_stop()
     print("gauche")
     if (line_sensor(LineSensor.R1)== WHITE and line_sensor(LineSensor.L1)==WHITE and line_sensor(LineSensor.M)==WHITE):
         V = True
     
-    droite()
-    sleep(800)
+    while not line_sensor(LineSensor.R1)== BLACK:
+        droite()
+    
     motor_stop()
     print("check fini ")
-    return True 
+    sleep(500)
+    return (V)
     
 
+def decision ():
+    mur_gauche = checkgauche()
+    
+    if mur_gauche == True  :
+        #gauche()
+        sleep(1100)
+        motor_stop()
+    #else:
+     #   mur_droite = checkdroite()
+    #if (mur_droite == True and mur_gauche == False):
+     #   droite()
+      #  sleep(1200)
+     #   motor_stop()
+    #elif (mur_droite == False and mur_gauche == False):
+     #   toutdroit()
+    #motor_stop()
+    #sleep(800)
+   # toutdroit()
+    
+        
 def toutdroit():
-    motor_run(Motor.LEFT, speed, Direction.FORWARD)
-    motor_run(Motor.RIGHT, speed, Direction.FORWARD)
-    
-    
+    while not (line_sensor(LineSensor.R1)== BLACK and line_sensor(LineSensor.L1)==BLACK and line_sensor(LineSensor.M)==BLACK):
+        motor_run(Motor.LEFT, speed, Direction.FORWARD)
+        motor_run(Motor.RIGHT, speed, Direction.FORWARD)
+        
+        sleep(50)
+    motor_stop()
 
 def droite():
     motor_run(Motor.LEFT, speed, Direction.FORWARD)
@@ -76,11 +102,15 @@ def gauche():
         #sleep(1000)
         #motor_stop()
         #toutdroit()
-        
-print(checkdroite())
-sleep (1000)
-print(checkgauche())
-    
+
+
+while True:
+    toutdroit()
+    decision()
+
+
+ 
+
     
 
 
