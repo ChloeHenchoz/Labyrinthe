1b66d858f3acb3b240a8ccfefdedf2e1ae01b6b1
ChloeHenchoz <chloe.hnchz@eduge.ch>
2025-05-14 11:45:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""Projet Laby 04.05.25 """
+"""Projet Laby 14.05.25 """
 #Imports
 
 from maprincess import *
@@ -29,6 +29,7 @@ def Tout_Droit(): # Tant que R2 + R1 sont sur la ligne et que M n'y est pas, on 
         
         
         motor_stop()
+        time1 = running_time()
         print(line_sensor_data_all())
         print(line_sensor_all())
         print("motor stop 1")
@@ -41,16 +42,22 @@ def Tout_Droit(): # Tant que R2 + R1 sont sur la ligne et que M n'y est pas, on 
             sleep(50)
             
         motor_stop()
+        time1 = running_time()
         print(line_sensor_data_all())
         print(line_sensor_all())
         print("motor stop 2")
         
-        while (line_sensor_all() == (1, 1, 0, 1, 0)):
+        while (line_sensor_all() == (1, 1, 0, 1, 0) or line_sensor_all() == (0, 0, 1, 1, 1)):
             led_rgb(Color.RED)
-            motor_run(Motor.ALL, speed, Direction.BACKWARD)
+            motor_run(Motor.LEFT, speed, Direction.BACKWARD)
+            motor_run(Motor.RIGHT, speed -20 , Direction.BACKWARD)
             sleep(50)
         motor_stop()
-            
+        time1 = running_time()
+        x = accelerometer.get_values()
+        if -20< x[0] < 20:
+        
+
             
     
 def Coin(): # si M + R1 + R2 sont sur une ligne, ce qui devrait être un coin, on tourne sur place jusqu'à que M ne soit plus sur la ligne
@@ -69,11 +76,13 @@ def Coin(): # si M + R1 + R2 sont sur une ligne, ce qui devrait être un coin, o
         print("stop")
         print(line_sensor_all())
         motor_stop()
+        time1 = running_time()
         
         if (line_sensor_all() == (0, 1, 0, 0, 0) or line_sensor_all() == (1, 1, 0, 0, 0) or line_sensor_all() == (0, 1, 0, 1, 1)): #  dans les deux config qui font que le robot s'arrête completement, on le fait reculer un peu pour qu'il reparte ( imite un coup de pouce) 
            motor_run(Motor.ALL, speed, Direction.BACKWARD)
            sleep(100)
         motor_stop()
+        time1 = running_time()
         
 #         while not ( line_sensor(LineSensor.R1) == BLACK and line_sensor(LineSensor.R2) == BLACK): # si R1 et/ou R2 ne sont plus sur la ligne on cherche à retrouver la ligne en avançant plus vite à gauche 
 #             led_rgb(Color.RED)
@@ -91,12 +100,14 @@ def Coin(): # si M + R1 + R2 sont sur une ligne, ce qui devrait être un coin, o
             motor_run(Motor.RIGHT, speed, Direction.BACKWARD)
             sleep(1000)
             motor_stop()
+            time1 = running_time()
            
         while (line_sensor_all() == (1, 1, 0, 1, 0)):
             led_rgb(Color.VIOLET)
             motor_run(Motor.ALL, speed, Direction.BACKWARD)
             sleep(50)
         motor_stop()
+        time1 = running_time()
             
 def Edge():  # si on rencontre un bord, c'est à dire que M et R1 ne sont plus sur la ligne mais R2 si, on tourne à droite pour en "faire le tour" jusqu'à que R2 +R1 soient sur la ligne mais pas M 
     
@@ -109,21 +120,22 @@ def Edge():  # si on rencontre un bord, c'est à dire que M et R1 ne sont plus s
             motor_run(Motor.LEFT, speed, Direction.FORWARD)
             
             sleep(50)
-        
+        motor_stop()
+        time1 = running_time()
+         
+def Creneau(): # si le robot s'est arrêté trop longtemps sans redémarrer, on lui fait faire un créneau pour débloquer la situation
+    x = accelerometer.get_values()
+        if -20< x[0] < 20: # si on est à l'arrêt 
+    time2 = running_time()
+    
+    
+  
+  
 def Fini():
     if 3 < ultrasonic() < 6 :
         Run = False
-        radio.send_bytes(bytes(0))
-        
-        
 
-def Radio_check():
-    
-    new_trame = radio.receive_bytes()
-    if new_trame:
-        msgRecu = trame_to_msg(new_trame, userId)
-        return msgRecu 
-
+        
         
 
 Run = True 
@@ -132,6 +144,7 @@ Run = True
 while True :
     
     while Run == True:
+ 
     
         Tout_Droit()
         sleep(50)
@@ -144,8 +157,7 @@ while True :
         
         Fini()
         
-    if Radio_check() == 0:  # Il faudrait faire commencer le deuxième robot en Run == False 
-            Run = True 
+    motor_stop()
     
         
     
